95dc6c60a2bd6226c44aa7e54fdb6b44c0044bab
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-04-13 20:10:38 +0400

    Created sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,16 @@
 class UsersController < ApplicationController
+  def new
+  end
 
-    def new
-        render "users/new"
-    end
-
-    def create
-        user = User.create!(
-            first_name: params[:first_name],
-            last_name: params[:last_name],
-            email: params[:email],
-            password: params[:password],
-            role: "customer",
-          )
-          session[:current_user_id] = user.id
-          redirect_to "/"
-    
-    end
-
-
-
+  def create
+    user = User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      role: "customer",
+    )
+    session[:current_user_id] = user.id
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+
   get "/", to: "home#index"
   resources :menus
   resources :menuitems
   resources :orders
   resources :orderitems
-  get "/signin" , to:"sessions#new", as: :new_sessions
+  get "/signin", to: "sessions#new", as: :new_sessions
   post "/signin", to: "sessions#create", as: :sessions
-  delete "/signout", to:"sessions#destroy", as: :destroy_session
+  delete "/signout", to: "sessions#destroy", as: :destroy_session
 end
