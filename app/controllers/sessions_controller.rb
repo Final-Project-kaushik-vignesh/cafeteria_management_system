2471d1640c90e5802c4b311a1e7179e8486f1dee
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-04-14 12:56:39 +0400

    Finished the login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,23 @@
 class SessionsController < ApplicationController
-    def new
-    
-    end
+  skip_before_action :ensure_user_logged_in
 
-    def create
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
-            session[:current_user_id] = user.id
-            redirect_to "/"
-        else
-            flash[:error] = "Your login attempt is Invaled. Please retry"
-            redirect_to new_sessions_path, warning: "Invalid User Id or Password"
-            
-        end
-    end
+  def new
+  end
 
-    def destroy
-        session[:current_user_id]=nil
-        @current_user = nil
-        redirect_to "/"
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:error] = "Your login attempt is Invaled. Please retry"
+      redirect_to new_sessions_path, warning: "Invalid User Id or Password"
     end
+  end
 
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
+  end
 end
