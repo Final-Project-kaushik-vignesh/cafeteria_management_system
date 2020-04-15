b45dcbb49931e3969ca2f306b0aa2fe1c3ae709c
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-04-15 15:29:45 +0530

    Redirection to edit menu-item added

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,16 +1,5 @@
 class MenuItemsController < ApplicationController
   def index
-<<<<<<< HEAD
-    render "index"
-  end
-
-  def create
-  end
-
-  def new
-    render "new"
-=======
     render plain: "This is success"
->>>>>>> 2c20e0cf4bae5a3ce4cf7a130c0e475b427393d7
   end
 end
