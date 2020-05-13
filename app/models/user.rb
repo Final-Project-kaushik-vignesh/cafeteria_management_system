1e259b2a91188be31796463b6103b0517fd02c0c
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-05-13 16:05:07 +0400

    Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
-  validates_presence_of :first_name, message: "This field is complusory: First name"
-  validates_presence_of :email, message: "This field is complusory: Email - Id"
+  validates_presence_of :first_name, message: "This field is compulsory: First name"
+  validates_presence_of :email, message: "This field is compulsory: Email - Id"
 end
