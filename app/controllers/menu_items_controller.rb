47ed7e322afb00f33fa1a0aba6745f988c69298a
Vignesh <vignesh.sankarn@gmail.com>
2020-06-20 17:50:39 +0400

    CSS Changes:

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -4,5 +4,27 @@ class MenuItemsController < ApplicationController
   def index
     active_menu = Menu.find_by(active_menu: true)
     @menu_item = MenuItem.of_menu(active_menu)
+    @order_items = OrderItem.all.where(order_id: current_order_id)
+  end
+  def update
+    id = params[:id]
+    order_item = OrderItem.find_by(id: id)
+    order_item.update(quantity: params[:quantity])
+    order_item.update(item_value: order_item.menu_item.price * params[:quantity].to_f)
+    order = Order.find_by(id: current_order_id)
+    order_items = OrderItem.all.where(order_id: order.id)
+    total_price = order_items.sum(:item_value)
+    order.update(total_price: total_price)
+    redirect_to menu_items_path
+  end
+  def destroy
+    id = params[:id]
+    order_item = OrderItem.find(id)
+    order_item.destroy
+    order = Order.find_by(id: current_order_id)
+    order_items = OrderItem.all.where(order_id: order.id)
+    total_price = order_items.sum(:item_value)
+    order.update(total_price: total_price)
+    redirect_to menu_items_path
   end
 end
