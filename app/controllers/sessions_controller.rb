016094d22bd1fa1682992cb2b74e7e584fd3b844
Vignesh <vignesh.sankarn@gmail.com>
2020-06-24 10:48:46 +0400

    CSS Changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       session[:current_user_id] = user.id
       redirect_to "/"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     user = User.new(
       first_name: params[:first_name].capitalize,
       last_name: params[:last_name].capitalize,
-      email: params[:email],
+      email: params[:email].downcase,
       password: params[:password],
       role: "customer",
     )
